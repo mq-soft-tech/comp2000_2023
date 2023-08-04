d2b09c65d357f35306497cb44fe0742afd8875a5
Alex Deane <alexrdeane@hotmail.com>
2023-08-05 02:43:42 +1000

    task4&5

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,116 @@
-<<<<<<< Updated upstream
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
-=======
 import javax.swing.*;
 import java.awt.*;
 
+import java.util.Random;
 
-public class Main extends JFrame
-{
-    public static void main(String[] args) throws Exception 
-    {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+
+public class Main extends JFrame {
+    public static void main(String[] args) throws Exception {
         Main window = new Main();
-
     }
 
-    class Canvas extends JPanel 
-    {
-        private int width = 720;
-        private int height = 720;
+    class Cell {
+        private int x;
+        private int y;
+        private int size;
+        private Color color;
 
-        public Canvas()
-        {
-            setPreferredSize(new Dimension(width, height));
+        public Cell(int x, int y, int size, Color color) {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+            this.color = color;
         }
 
-        @Override
-        public void paint(Graphics g)
-        {
-            for(int i = 10; i < 710; i+=35)
-            {
-                for(int j = 10; j < 710; j+=35)
-                {
-                    g.setColor(Color.white);
-                    g.fillRect(i, j, 35, 35);
-                    g.setColor(Color.black);
-                    g.drawRect(i, j, 35, 35);
+        public void paint(Graphics g) {
+            g.setColor(color.white);
+            g.fillRect(x, y, size, size);
+            g.setColor(Color.black);
+            g.drawRect(x, y, size, size);
+        }
+    }
+
+    class Grid extends JPanel {
+        private int width = 720;
+        private int height = 720;
+        private int cellSize = 35;
+        private Cell[][] cells;
+        int padding = 10;
+
+        private Cell mouseOverCell = null;
+
+        private int xAmount = 20, yAmount = 20;
+
+        public Grid() {
+            setPreferredSize(new Dimension(width, height));
+            createGrid();
+            addMouseListener(new MouseListen());
+            addMouseMotionListener(new MouseListen());
+        }
+
+        public void createGrid() {
+            cells = new Cell[xAmount][yAmount];
+            Random randomCol = new Random();
+            int cMin = 0;
+            int cMax = 256;
+            for (int i = 0; i < xAmount; i++) {
+                for (int j = 0; j < yAmount; j++) {
+                    int r = randomCol.nextInt(cMax - cMin) + cMin;
+                    int g = randomCol.nextInt(cMax - cMin) + cMin;
+                    int b = randomCol.nextInt(cMax - cMin) + cMin;
+                    cells[i][j] = new Cell(padding + i * cellSize, padding + j * cellSize, cellSize,
+                            new Color(r, g, b));
                 }
             }
         }
+
+        @Override
+        public void paint(Graphics g) {
+            super.paintComponent(g);
+            for (int i = 0; i < xAmount; i++) {
+                for (int j = 0; j < yAmount; j++) {
+                    cells[i][j].paint(g);
+                }
+            }
+
+            if (mouseOverCell != null) {
+                g.setColor(Color.black);
+                g.fillRect(mouseOverCell.x, mouseOverCell.y, mouseOverCell.size, mouseOverCell.size);
+            }
+        }
+
+        private class MouseListen extends MouseAdapter {
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                int mouseX = e.getX();
+                int mouseY = e.getY();
+
+                mouseOverCell = MouseOverCell(mouseX, mouseY);
+                repaint();
+            }
+        }
+
+        private Cell MouseOverCell(int x, int y) {
+            for (int i = 0; i < xAmount; i++) {
+                for (int j = 0; j < yAmount; j++) {
+                    Cell cell = cells[i][j];
+                    if (x >= cell.x && x < cell.x + cell.size && y >= cell.y && y < cell.y + cell.size) {
+                        return cell;
+                    }
+                }
+            }
+            return null;
+        }
     }
 
-    private Main()
-    {
+    private Main() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Canvas canvas = new Canvas();
-        this.setContentPane(canvas);
+        Grid grid = new Grid();
+        this.setContentPane(grid);
         this.pack();
         this.setVisible(true);
->>>>>>> Stashed changes
     }
 }
-
